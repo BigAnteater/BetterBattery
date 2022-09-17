069002c175177182b22af2605b6db35cde01b336
Michal <94299562+misopog@users.noreply.github.com>
2022-09-17 12:56:13 +0200

    Complete rewrite
    fixing some issues, and rewriting the entire script in dialog (ncurses)

--- jumpstart.sh
+++ jumpstart.sh
@@ -6,18 +6,17 @@ if [ $EUID -ne 0 ]
 		exit 1
 fi
 
-echo "Welcome fellow laptop user"
-echo "make sure you have yay installed"
-sleep 2s
-echo "This script will automatically set up your laptop to have a much higher battery life than before."
-sleep 2s
+
+sudo pacman -Syu dialog
+dialog --title 'Hello,' --msgbox 'This script will (hopefully) make the battery life on your laptop better' 20 50
+dialog --title 'WARNING!' --msgbox 'MAKE SURE YOU USE ARCH LINUX AND HAVE YAY FOR THIS!' 20 50
+dialog --msgbox 'This script will automatically set up your laptop to have a much higher battery life than before.' 20 50
 clear
-echo "First we are going to install some needed programs."
-sleep 2s
-clear
-echo "TLP is a very popular battery life tool which makes optimization simple"
+dialog --msgbox 'First we are going to install some needed programs.' 20 50
 sleep 1s
-pacman -S tlp
+dialog --msgbox  'TLP is a very popular battery life tool which makes optimization simple' 20 50
+sleep 1s
+pacman -Sy tlp -y
 sleep 1s
 systemctl mask systemd-rfkill.service
 systemctl mask systemd-rfkill.socket
@@ -26,41 +25,33 @@ sleep 1s
 systemctl start tlp.service
 sleep 1s
 clear
-echo "now we are going to be automatically setting up the configs"
-sleep 2s
+dialog --msgbox 'Now we are going to be automatically setting up the configs, dont worry, this will back up your current config' 20 50
 clear
-echo "mv /etc/tlp.conf /etc/tlp.conf/old"
 mv /etc/tlp.conf /etc/tlp.conf.old
-sleep 2s
-echo "cp config/tlp.conf /etc/tlp.conf"
-cp configs/tlp.conf /etc/tlp.conf
-sleep 5s
+mv configs/tlp.conf /etc/tlp.conf
 clear
-echo "TLP has been successfully configured!"
-sleep 4s
+dialog --msgbox 'TLP has been successfully configured!' 20 50
 clear
-echo "Now it's time to configure auto-cpufreq."
-sleep 1s
-echo "Auto-cpufreq is a tool which governs, and configures your CPU to only use the ammount of power it actually needs on battery life."
-sleep 4s
+dialog --msgbox 'Now its time to configure auto-cpufreq.' 20 50
+dialog --msgbox 'Auto-cpufreq is a tool which governs, and configures your CPU to only use the ammount of power it actually needs on battery life.' 20 50
+dialog --msgbox 'This will compile the program so it may take a while' 20 50
 clear
-runuser -l $USER -c 'yay -S auto-cpufreq'
+runuser -l $USER -c 'yay -Sy auto-cpufreq'
 sleep 1s
 systemctl enable auto-cpufreq
 systemctl start autocpufreq
 sleep 4s
 clear
-echo "renaming old auto-cpufreq config. Don't poop your pants if you get an error, this only renames existing configs"
+dialog --msgbox 'Renaming old auto-cpufreq config. Dont poop your pants if you get an error, this only renames existing configs' 20 50
 mv /etc/auto-cpufreq.conf /etc/auto-cpufreq.conf.old
-sleep 4s
-echo "cp configs/auto-cpufreq.conf /etc"
-cp configs/auto-cpufreq.conf /etc/auto-cpufreq.conf
+sleep 1s
+mv configs/auto-cpufreq.conf /etc/auto-cpufreq.conf
 sleep 4s
 clear
-echo "Battery life has been successfully configured!"
+dialog --msgbox 'Battery life has been successfully configured!' 20 50
 sleep 1s
 chmod +x uninstall.sh
-echo "run ./uninstall.sh to revert all changes."
+dialog --msgbox 'run ./uninstall.sh to revert all changes.' 20 50
 sleep 2s
 clear
 echo "A reboot is needed,type 'y' to reboot now and 'n' to reboot later"
